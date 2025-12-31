7589a046d243c9d8908eba34546981d3cd06bb1e
gereiz <georgie.reis@outlook.com>
2025-12-31 08:13:51 -0300

    Ajustes no seeder da landing page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PlanSeeder::class,
             AdminUserSeeder::class,
+            LandingPageSeeder::class,
+            PlansPageSeeder::class,
         ]);
     }
 }
